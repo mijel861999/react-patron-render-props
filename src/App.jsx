04f592162e9a06d976fdd0415bdce84e4339c1d4
Miguel Castillo <mijel.dev@gmail.com>
2023-08-06 02:24:42 -0500

    feat: agregando renderizado con provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,20 @@ import DataProvider from './components/DataProvider'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>Datos desde el data provider</h1>
+      <DataProvider
+        render={(data) => (
+          <ul>
+            {
+              data.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))
+            }
+          </ul>
+        )}
+      />
+    </div>
   )
 }
 
